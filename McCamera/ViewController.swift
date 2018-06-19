82547a8d1935d01db0bbca4b62b233ee401fe1b5
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-06-19 12:28:42 -0700

    Initial Commit

---
+++ McCamera/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  McCamera
+//
+//  Created by Cyril Garcia on 6/19/18.
+//  Copyright © 2018 ByCyril. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
